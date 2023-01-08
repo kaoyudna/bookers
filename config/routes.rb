f7d0d599edc916568165c8cd4e9374da800ef486
EC2 Default User <ec2-user@ip-172-31-3-94.ap-northeast-1.compute.internal>
2023-01-08 12:46:31 +0000

    [Add]新規投稿ページ及び詳細ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'books'=>'books#index'
-  get 'books/:id'=>'books#show'
-  get 'books/:id/edit'=>'books#edit'
+  post 'books'=>'books#create'
+  get 'books/:id'=>'books#show',as: 'book'
+  get 'books/:id/edit'=>'books#edit',as: 'edit_book'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
