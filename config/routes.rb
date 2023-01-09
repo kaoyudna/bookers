dd285718d4eef88729d0680d665d6b14f245a4ec
EC2 Default User <ec2-user@ip-172-31-3-94.ap-northeast-1.compute.internal>
2023-01-09 09:46:23 +0000

    [Add]新規投稿と更新フォームにバリデーション機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'books'=>'books#index'
-  post 'books'=>'books#create'
-  get 'books/:id'=>'books#show',as: 'book'
-  get 'books/:id/edit'=>'books#edit',as: 'edit_book'
-  patch 'book/:id/edit'=>'books#update',as: 'update_book'
-  delete "books/:id"=>'books#destroy',as: 'destroy_book'
-  get 'homes/top'
+  resources :books
+  #get 'books'=>'books#index'
+  #post 'books'=>'books#create'
+  #get 'books/:id'=>'books#show',as: 'book'
+  #get 'books/:id/edit'=>'books#edit',as: 'edit_book'
+  #patch 'book/:id/edit'=>'books#update',as: 'update_book'
+  #delete "books/:id"=>'books#destroy',as: 'destroy_book'
+  #get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
